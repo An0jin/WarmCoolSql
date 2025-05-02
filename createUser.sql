c5612cca9fea5a0f2fbd3307174d40ae7fda01a7
안영진 <158057603+anyoungjin20040106@users.noreply.github.com>
2025-05-02 13:44:14 +0900

    Update createUser.sql

--- createUser.sql
+++ createUser.sql
@@ -1,7 +1,7 @@
 CREATE Table "user"(user_id VARCHAR(255) PRIMARY KEY,
 pw TEXT NOT NULL ,
 name TEXT NOT NULL, 
-birthday date, 
+year int not NULL, 
 gender Text Check(gender IN ('남자','여자')) NOT NULL,
 hex_code varchar(7),
 Foreign Key (hex_code) REFERENCES lipstick(hex_code));
