d35620219fc7b7b3b777079d821b902130d72f83
안영진 <158057603+An0jin@users.noreply.github.com>
2026-01-09 02:49:32 +0900

    성별, 태어난 연도 데이터 추가

--- createUser.sql
+++ createUser.sql
@@ -2,4 +2,6 @@ CREATE Table "user"(email TEXT PRIMARY KEY,
 pw TEXT NOT NULL ,
 name TEXT NOT NULL, 
 hex_code TEXT,
+sex TEXT,
+year TEXT,
 Foreign Key (hex_code) REFERENCES lipstick(hex_code));
--- v_user_lipstick.sql
+++ v_user_lipstick.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE VIEW v_user_lipstick
-AS SELECT user.email,
-    user.name,
-    user.pw,
+AS SELECT "user".email,
+    "user".name,
+    "user".pw,
+    "user".sex,
+    "user".year,
     lipstick.hex_code,
     lipstick.color_id,
     lipstick.cname
-   FROM user
-     LEFT JOIN lipstick ON user.hex_code = lipstick.hex_code;
+   FROM "user"
+     LEFT JOIN lipstick ON "user".hex_code = lipstick.hex_code;
